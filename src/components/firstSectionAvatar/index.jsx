ca12e8d8b19735ea9d7bf2d88ebb5c0d35dfe9fa
Ivan <ivancheresky@gmail.com>
2022-05-15 00:37:38 -0300

    Fix avatar container and buttons styling

--- src/components/firstSectionAvatar/index.jsx
+++ src/components/firstSectionAvatar/index.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function ArtistAvatar({ title, image, play }) {
+  const [isHovering, setIsHovering] = useState(false);
+
+  const onMouseEnter = () => {
+    setIsHovering(true);
+  };
+
+  const onMouseLeave = () => {
+    setIsHovering(false);
+  };
+
   return (
-    <div className="avatar">
-      <div className="title-image-play-container">
+    <div
+      className="avatar"
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      <img className="image" src={image} alt="artist" />
+      <div className="title-button-container">
         <div className="title"> {title} </div>
-        <div>
-          <img className="image" src={image} alt="artist" />
-        </div>
-        <div className="play">{play}</div>
+        {isHovering && <div className="play">Playing button</div>}
       </div>
     </div>
   );
--- src/components/landingPage/index.jsx
+++ src/components/landingPage/index.jsx
@@ -7,12 +7,8 @@ import "./styles.css";
 export default function index() {
   return (
     <div className="landing-page-container">
-      <span>&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;</span>
       <button className="upgrade-button"> Upgrade </button>
-      <span>&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;</span>
       <WelcomeBar /> <FirstSection />
-      <span>&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;</span>
-      <span>&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;</span>
       <SecondSection />
     </div>
   );
